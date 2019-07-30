72434a5bc9317a11e39c5de0659ffeacfc714d51
渋谷 珠己 <1111918@PC0533.local>
2019-07-30 15:41:02 +0900

    Revert "recreated"
    This reverts commit 930d8a673960f3ba257eaec2de5be70754275d6e.

--- db/migrate/20190717030931_create_tasks.rb
+++ db/migrate/20190717030931_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.string :contents
+      t.text :contents
       t.date :start
       t.date :dl
       t.string :type
