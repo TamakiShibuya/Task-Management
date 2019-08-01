4b41750de35778443f3ef748136362c40d73048d
渋谷 珠己 <1111918@PC0533.local>
2019-08-01 18:08:03 +0900

    updating the simple implementation

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,1 +1,3 @@
-Rails.applocation.config.i18n.default_locale = :ja
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+I18n.config.available_locales = :ja
+I18n.default_locale = :ja
