dab13654cfdcfcc0f0c82aadb63294a4ee109b5a
渋谷 珠己 <1111918@PC0533.local>
2019-08-07 18:52:25 +0900

    create spec

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,1 @@
-I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-I18n.config.available_locales = :ja
-I18n.default_locale = :ja
+Rails.application.config.i18n.default_locale = :ja
