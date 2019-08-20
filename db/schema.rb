ec6c2579cf2d038df38b22a9d4692de395662b9a
渋谷 珠己 <1111918@PC0533.local>
2019-08-20 17:42:14 +0900

    update deadline

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_030931) do
+ActiveRecord::Schema.define(version: 2019_08_15_033645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "contents"
-    t.date "dl"
+    t.date "deadline"
     t.string "type"
     t.integer "prior"
     t.integer "prog"
