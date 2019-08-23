45c939cf1b658678f05242b20da741f37c2b6421
渋谷 珠己 <1111918@PC0533.local>
2019-08-23 12:05:10 +0900

    correct indent

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,8 +14,11 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
 
-    @task.save if redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
-    render :new
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
