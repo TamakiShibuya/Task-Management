8d490e86c39a69bfd8714ecbb91c79699d0b3b51
渋谷 珠己 <1111918@PC0533.local>
2020-04-08 15:19:37 +0900

    PR修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
-  # skip_before_action :login_reqired
+  skip_before_action :login_reqired
   def new
   end
 
   def create
-    user= User.find_by(email: session_params[:email])
+    user = User.find_by(email: session_params[:email])
 
-    if users&.authenticate(session_params[:password])
+    if user&.authenticate(session_params[:password])
       session[:user_id] = user.user_id
       redirect_to root_url, notice: 'ログインしました'
     else
