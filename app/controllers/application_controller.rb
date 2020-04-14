8315fc12e1957d5d53e5caa73f8b828a1a737e0f
渋谷 珠己 <1111918@PC0533.local>
2020-04-15 01:14:56 +0900

    エラーが表示されるよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,19 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :login_required
 
+  class Forbidden < ActionController::ActionControllerError
+    end
+  
+    rescue_from Forbidden,                      with: :rescue403
+  
+    rescue_from InternalServer,                 with: :rescue500
+    rescue_from ActiveRecord::RecordNotFound,   with: :rescue404
+    rescue_from ActionController::RoutingError, with: :rescue404
+  
+    def routing_error
+      raise ActionController::RoutingError, params[:path]
+    end
+
   private
 
   def current_user
@@ -9,6 +22,18 @@ class ApplicationController < ActionController::Base
   end
 
   def login_required
-    redirect_to login_url unless current_user
+    redirect_to login_path unless current_user
+  end
+
+  def rescue403
+    render template: 'errors/forbidden', status: 403
+  end
+
+  def rescue404
+    render template: 'errors/not_found', status: 404
+  end
+
+  def rescue500
+    render template: 'errors/internal_sever_error', status: 500
   end
 end
